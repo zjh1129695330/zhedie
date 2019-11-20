6f57420bc399607675dc81be464d89e9da52f435
zjh1129695330 <1129695330@qq.com>
2019-11-20 20:36:14 +0800

    “第一次提交”

--- js/js.js
+++ js/js.js
@@ -8,7 +8,7 @@ var ul1=a1.getElementsByClassName('ul1');
  for (var i=0;i<ul1.length;i++){
 	c1[i].index=i;
 	ul1[i].index1=i;
-	var c=c1[i].onclick=function(){
+	c1[i].onclick=function(){
 		index=this.index;
 	if(ul1[index].style.display=='none'){
 		for (var i=0;i<ul1.length;i++){
